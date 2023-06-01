349b35e083b59cc785e4b13e4e6dee3ce233aa27
March360 <zaynezekiel16@gmail.com>
2023-06-01 13:11:35 -1000

    up5

--- javascript.js
+++ javascript.js
@@ -151,23 +151,23 @@ $(document).ready(function(){
 
 
 
-// ScrollReveal({
-//   distance: '60px',
-//   duration: '2500',
-//   delay: 400
-//   });
+ScrollReveal({
+  distance: '20px',
+  duration: '2500',
+  delay: 400
+  });
     
-//     ScrollReveal().reveal('.title', { delay: 500, origin: 'bottom'},{mobile: false});
-//     ScrollReveal().reveal('.about .about-image', { delay: 500, rotate:{x:40 ,z:40} },{mobile: false});
-//     ScrollReveal().reveal('.textdiv', { delay: 500, origin: 'bottom'},{mobile: false});
-//     ScrollReveal().reveal('.services .column', { delay: 500, origin: 'bottom' },{mobile: false});
-//     ScrollReveal().reveal('.card-reveal-1', { delay: 500, origin: 'bottom' },{mobile: false});
-//     ScrollReveal().reveal('.revealDiv', { delay: 500, origin: 'left'},{mobile: false});
-//     ScrollReveal().reveal('.skills-desc', { delay: 500, origin: 'left' },{mobile: false});
-//     ScrollReveal().reveal('.skills-Info', { delay: 500, origin: 'bottom' },{mobile: false});
-//     ScrollReveal().reveal('.contact-desc', { delay: 500, origin: 'left' },{mobile: false});
-//     ScrollReveal().reveal('.icons', { delay: 500, origin: 'bottom' },{mobile: false});
-//     ScrollReveal().reveal('.message', { delay: 500, origin: 'bottom' },{mobile: false});
+    ScrollReveal().reveal('.title', { delay: 500, origin: 'bottom'},{mobile: false});
+    ScrollReveal().reveal('.about .about-image', { delay: 500, rotate:{x:40 ,z:40} },{mobile: false});
+    ScrollReveal().reveal('.textdiv', { delay: 500, origin: 'bottom'},{mobile: false});
+    ScrollReveal().reveal('.services .column', { delay: 500, origin: 'bottom' },{mobile: false});
+    ScrollReveal().reveal('.card-reveal-1', { delay: 500, origin: 'bottom' },{mobile: false});
+    ScrollReveal().reveal('.revealDiv', { delay: 500, origin: 'left'},{mobile: false});
+    ScrollReveal().reveal('.skills-desc', { delay: 500, origin: 'left' },{mobile: false});
+    ScrollReveal().reveal('.skills-Info', { delay: 500, origin: 'bottom' },{mobile: false});
+    ScrollReveal().reveal('.contact-desc', { delay: 500, origin: 'left' },{mobile: false});
+    ScrollReveal().reveal('.icons', { delay: 500, origin: 'bottom' },{mobile: false});
+    ScrollReveal().reveal('.message', { delay: 500, origin: 'bottom' },{mobile: false});
 
   jQuery(document).ready(function(){
   
