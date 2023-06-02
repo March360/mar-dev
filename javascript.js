052eb99e588ff1ff0b687c4103a550bdc1193713
March360 <zaynezekiel16@gmail.com>
2023-06-01 14:31:34 -1000

    commit

--- javascript.js
+++ javascript.js
@@ -143,27 +143,26 @@ $(document).ready(function(){
    
 });
 
-window.sr = ScrollReveal(
-  // distance: '60px',
-  // duration: '2500',
-  // delay: 400
-
-  sr.reveal('.title', { delay: 500, origin: 'bottom'})
+function scrf(){
+  window.sr = ScrollReveal({
+    distance: '60px',
+    duration: '2500',
+    delay: 400
+    });
+    
+    sr.reveal('.title', { delay: 500, origin: 'bottom'});
+    sr.reveal('.about .about-image', { delay: 500, rotate:{x:40 ,z:40} });
+    sr.reveal('.textdiv', { delay: 500, origin: 'bottom'});
+    sr.reveal('.services .column', { delay: 500, origin: 'bottom' });
+    sr.reveal('.card-reveal-1', { delay: 500, origin: 'bottom' });
+    sr.reveal('.revealDiv', { delay: 500, origin: 'left'});
+    sr.reveal('.skills-desc', { delay: 500, origin: 'left' });
+    sr.reveal('.skills-Info', { delay: 500, origin: 'bottom' });
+    sr.reveal('.contact-desc', { delay: 500, origin: 'left' });
+    sr.reveal('.icons', { delay: 500, origin: 'bottom' });
+    sr.reveal('.message', { delay: 500, origin: 'bottom' });
   
- );
-  
-  // sr.reveal('.title', { delay: 500, origin: 'bottom'});
-  // sr.reveal('.about .about-image', { delay: 500, rotate:{x:40 ,z:40} });
-  // sr.reveal('.textdiv', { delay: 500, origin: 'bottom'});
-  // sr.reveal('.services .column', { delay: 500, origin: 'bottom' });
-  // sr.reveal('.card-reveal-1', { delay: 500, origin: 'bottom' });
-  // sr.reveal('.revealDiv', { delay: 500, origin: 'left'});
-  // sr.reveal('.skills-desc', { delay: 500, origin: 'left' });
-  // sr.reveal('.skills-Info', { delay: 500, origin: 'bottom' });
-  // sr.reveal('.contact-desc', { delay: 500, origin: 'left' });
-  // sr.reveal('.icons', { delay: 500, origin: 'bottom' });
-  // sr.reveal('.message', { delay: 500, origin: 'bottom' });
-
+}
     
 
   jQuery(document).ready(function(){
