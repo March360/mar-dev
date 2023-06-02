28058f4e17f561c278a8fc98f29d03d0a669d9dd
March360 <zaynezekiel16@gmail.com>
2023-06-01 14:19:02 -1000

    reveal enable

--- javascript.js
+++ javascript.js
@@ -147,7 +147,8 @@ window.sr = ScrollReveal({
   distance: '60px',
   duration: '2500',
   delay: 400
-  });
+  },
+  sr.reveal('.title', { delay: 500, origin: 'bottom'}));
   
   // sr.reveal('.title', { delay: 500, origin: 'bottom'});
   // sr.reveal('.about .about-image', { delay: 500, rotate:{x:40 ,z:40} });
