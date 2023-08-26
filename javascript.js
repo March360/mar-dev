5b74501db2001767304ed9408f8726517b681fb4
March360 <zaynezekiel16@gmail.com>
2023-08-25 17:34:15 -1000

    updated light box

--- javascript.js
+++ javascript.js
@@ -224,16 +224,40 @@ ScrollReveal({
         $('.pl,.preloadingName').fadeToggle();
       }, 1700);
       
-      // let galleryBtn = document.querySelector('#gallery-btn');
-      // let currentItem = 3;
-
-      // galleryBtn.onclick = () =>{
-      //   let boxes = [...document.querySelectorAll('.project .pro-container .card')];
-      //   for (var i = currentItem; i < currentItem + 3; i++){
-      //      boxes[i].style.display = 'inline-block';
-      //   }
-      //   currentItem += 3;
-      //   if(currentItem >= boxes.length){
-      //     galleryBtn.style.display = 'none';
-      //   }
-      // }
+      // modal area
+      function openModal() {
+        document.getElementById("myModal").style.display = "block";
+      }
+      
+      function closeModal() {
+        document.getElementById("myModal").style.display = "none";
+      }
+      
+      var slideIndex = 1;
+      showSlides(slideIndex);
+      
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
+      
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+      
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo-2");
+        var captionText = document.getElementById("caption");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active-2", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active-2";
+        captionText.innerHTML = dots[slideIndex-1].alt;
+      }
